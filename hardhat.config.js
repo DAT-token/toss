568c1a7bb8dbe287db24273e516224b129750f4c
remche <remche@remche.org>
2024-06-03 16:56:23 +0200

    set ignition fees

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,11 @@ module.exports = {
     debank: {
       url: "https://sepolia-rpc.testnet.debank.com",
       chaindId: "20240324",
-      accounts: [vars.get("DBK_PRIV_KEY")]
+      accounts: [vars.get("DBK_PRIV_KEY")],
+      ignition: {
+	maxFeePerGasLimit: 50_000_000_000n, // 50 gwei
+        maxPriorityFeePerGas: 2_000_000_000n, // 2 gwei
+      }
     }
   },
   solidity: "0.8.24",
